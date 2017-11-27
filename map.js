65b5f0eab2689023fd9b2060ed92a5cf36ffe6b0
ferhammaren <fernanda.meza.lopez@gmail.com>
2017-11-27 10:56:35 -0800

    lmao separate this shit

---
+++ map.js
@@ -0,0 +1,10 @@
+
+        function initMap(){
+                  var satelliteMap;
+            satelliteMap=new google.maps.Map(document.getElementById('satelliteMap'),{
+                center:{lat:38.317238,lng:140.892181},
+                zoom:17,
+                mapTypeId:'hybrid'
+            });
+        
+        }
